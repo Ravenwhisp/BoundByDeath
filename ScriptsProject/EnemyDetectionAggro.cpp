98f42518ca563050f15151b8dd3077de3337bc69
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-04-01 18:09:58 +0200

    Added the script skeleton

--- ScriptsProject/EnemyDetectionAggro.cpp
+++ ScriptsProject/EnemyDetectionAggro.cpp
@@ -5,7 +5,10 @@ static const ScriptFieldInfo enemyDetectionAggroFields[] =
 {
 	{ "Detection Radius", ScriptFieldType::Float, offsetof(EnemyDetectionAggro, m_detectionRadius), { 0.0f, 50.0f, 0.1f } },
 	{ "Lose Aggro Delay", ScriptFieldType::Float, offsetof(EnemyDetectionAggro, m_loseAggroDelay), { 0.0f, 10.0f, 0.1f } },
-	{ "Debug Enabled", ScriptFieldType::Bool, offsetof(EnemyDetectionAggro, m_debugEnabled) }
+	{ "Debug Enabled", ScriptFieldType::Bool, offsetof(EnemyDetectionAggro, m_debugEnabled) },
+
+	{ "Player 1 Transform", ScriptFieldType::ComponentRef, offsetof(EnemyDetectionAggro, m_player1Transform), {}, {}, { ComponentType::TRANSFORM } },
+	{ "Player 2 Transform", ScriptFieldType::ComponentRef, offsetof(EnemyDetectionAggro, m_player2Transform), {}, {}, { ComponentType::TRANSFORM } }
 };
 
 IMPLEMENT_SCRIPT_FIELDS(EnemyDetectionAggro, enemyDetectionAggroFields)
@@ -18,6 +21,26 @@ void EnemyDetectionAggro::Start()
 }
 
 void EnemyDetectionAggro::Update()
+{
+	updateAggroState();
+}
+
+bool EnemyDetectionAggro::canDetectTarget() const
+{
+	return false;
+}
+
+void EnemyDetectionAggro::enterAggro()
+{
+
+}
+
+void EnemyDetectionAggro::exitAggro()
+{
+
+}
+
+void EnemyDetectionAggro::updateAggroState()
 {
 
 }
--- ScriptsProject/EnemyDetectionAggro.h
+++ ScriptsProject/EnemyDetectionAggro.h
@@ -19,4 +19,23 @@ public:
 	float m_detectionRadius = 10.0f;
 	float m_loseAggroDelay = 2.0f;
 	bool m_debugEnabled = true;
+
+	Component* m_player1Transform = nullptr;
+	Component* m_player2Transform = nullptr;
+
+private:
+	GameObject* m_target = nullptr; // current target
+	bool m_isAggro = false; // is enemy in aggro state
+	bool m_canSeeTarget = false; // is target detected
+	float m_timeSinceLastSeen = 0.0f; // how much time has passed since detected target
+	Vector3 m_lastKnownTargetPosition = Vector3(0.0f, 0.0f, 0.0f); // last known position of the target
+
+private:
+	bool isTargetValid() const;
+	bool isTargetInDetectionRange() const;
+	bool canDetectTarget() const;
+
+	void enterAggro();
+	void exitAggro();
+	void updateAggroState();
 };
