12a665fe90fbf8ee2e70832544a58f51ace543c2
Jordi <pimoreno.j@outlook.es>
2026-04-05 18:48:03 +0200

    debug damage trigger bug solved

--- ScriptsProject/DebugDamageTrigger.h
+++ ScriptsProject/DebugDamageTrigger.h
@@ -22,7 +22,7 @@ public:
     float m_damageAmount = 25.0f;
     float m_healAmount = 25.0f;
 
-    float m_playerIndex = 0;
+    int m_playerIndex = 0;
 private:
     Damageable* m_damageable = nullptr;
 };
