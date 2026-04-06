9a0047239db31d0f347a0dc3cd3668f11e32310a
David Varo <david.vama21@gmail.com>
2026-04-06 21:59:28 +0200

    Base for Lyriel's charged attack/ shooter enemy

---
+++ ScriptsProject/Shooter.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "Shooter.h"
+
+static const ScriptFieldInfo shooterFields[] =
+{
+    { "Charge Time", ScriptFieldType::Float, offsetof(Shooter, m_chargeTime), { 0.0f, 150.f, 0.1f } },
+    { "Target", ScriptFieldType::ComponentRef, offsetof(Shooter, m_target), {}, {}, { ComponentType::TRANSFORM } },
+    {"Prefab to instantiate", ScriptFieldType::String, offsetof(Shooter, m_projectileToInstantiate)  },
+    { "Spawning relative point", ScriptFieldType::Vec3, offsetof(Shooter, m_spawnRelativePoint) }
+};
+
+IMPLEMENT_SCRIPT_FIELDS(Shooter, shooterFields)
+
+Shooter::Shooter(GameObject* owner)
+    : Script(owner)
+{
+    m_objectTransform = GameObjectAPI::getTransform(owner);
+}
+
+void Shooter::Start()
+{
+    m_isCharging = m_toReleaseCharge = false;
+    m_currentChargeTime = 0;
+}
+
+void Shooter::Update()
+{
+    if (m_isCharging)
+    {
+
+        if (m_toReleaseCharge) 
+        {
+            // release code
+
+
+            m_isCharging = m_toReleaseCharge = false;
+        }
+        else 
+        {
+            if (m_currentChargeTime == m_chargeTime) return;
+
+            m_currentChargeTime += Time::getDeltaTime();
+            if (m_currentChargeTime > m_chargeTime) m_currentChargeTime = m_chargeTime;
+        }
+
+    }
+}
+
+// MAYBE ADD IFs SO THAT THESE DO NOT DO ANYTHING BEYOND WHEN IT IS REQUIRED?
+void Shooter::startCharge()
+{
+    m_isCharging = true;
+}
+
+void Shooter::releaseCharge()
+{
+    m_toReleaseCharge = true;
+}
+
+void Shooter::releaseImmediately()
+{
+    m_isCharging = true;
+    m_toReleaseCharge = true;
+}
+
+
+IMPLEMENT_SCRIPT(Shooter)
---
+++ ScriptsProject/Shooter.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ScriptAPI.h"
+
+class Shooter : public Script
+{
+    DECLARE_SCRIPT(Shooter)
+
+public:
+    explicit Shooter(GameObject* owner);
+
+    void Start() override;
+    void Update() override;
+
+    ScriptFieldList getExposedFields() const override;
+
+    // To control behaviour INDEPENDENTLY of the input (and maybe use with AI)
+    void startCharge();
+    void releaseCharge();
+    void releaseImmediately(); // to allow just a basic shot
+
+    bool isCharging() const { return m_isCharging; }
+
+    float m_chargeTime = 2; // in seconds!
+
+    ScriptComponentRef<Transform> m_target; // the object to hit
+
+    std::string m_projectileToInstantiate = "";
+    Vector3 m_spawnRelativePoint = Vector3(0.f, 0.f, 0.f); // respect to target direction
+
+
+private:
+
+    Transform* m_objectTransform; // the current object
+
+    bool m_isCharging;
+    float m_currentChargeTime;
+
+    bool m_toReleaseCharge;
+};
+
