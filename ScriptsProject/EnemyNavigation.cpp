bd3ab2f2aa87c11287506d9367a805ee557459e2
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-04-08 19:18:36 +0200

    added exposed variables

--- ScriptsProject/EnemyNavigation.cpp
+++ ScriptsProject/EnemyNavigation.cpp
@@ -1,8 +1,14 @@
 #include "pch.h"
 #include "EnemyNavigation.h"
+#include "EnemyDetectionAggro.h"
 
 static const ScriptFieldInfo EnemyNavigationFields[] =
 {
+	{ "Combat Range", ScriptFieldType::Float, offsetof(EnemyNavigation, m_combatRange), { 0.0f, 50.0f, 0.1f } },
+	{ "Move Speed", ScriptFieldType::Float, offsetof(EnemyNavigation, m_moveSpeed), { 0.0f, 50.0f, 0.1f } },
+	{ "Turn Speed", ScriptFieldType::Float, offsetof(EnemyNavigation, m_turnSpeed), { 0.0f, 50.0f, 0.1f } },
+	{ "Interval", ScriptFieldType::Float, offsetof(EnemyNavigation, m_intervalRepath), { 0.0f, 50.0f, 0.1f } },
+	{ "Debug Enabled", ScriptFieldType::Bool, offsetof(EnemyNavigation, m_debugEnabled) }
 };
 
 IMPLEMENT_SCRIPT_FIELDS(EnemyNavigation, EnemyNavigationFields)
@@ -14,10 +20,18 @@ EnemyNavigation::EnemyNavigation(GameObject* owner)
 
 void EnemyNavigation::Start()
 {
+	Script* script = GameObjectAPI::getScript(getOwner(), "EnemyDetectionAggro");
+	m_enemyDetectionAggro = dynamic_cast<EnemyDetectionAggro*>(script);
+
+	if (!m_enemyDetectionAggro)
+	{
+		Debug::error("EnemyDetectionAggro script not found!");
+	}
 }
 
 void EnemyNavigation::Update()
 {
+
 }
 
 IMPLEMENT_SCRIPT(EnemyNavigation)
--- ScriptsProject/EnemyNavigation.h
+++ ScriptsProject/EnemyNavigation.h
@@ -20,11 +20,20 @@ public:
 
     void Start() override;
     void Update() override;
-    void drawGizmo() override;
+    //void drawGizmo() override;
 
     ScriptFieldList getExposedFields() const override;
 
 public:
-    ScriptComponentRef<EnemyDetectionAggro> m_enemyDetectionAggro;
+    float m_combatRange = 2.0f;
+    float m_moveSpeed = 1.0f;
+    float m_turnSpeed = 1.0f;
+    float m_intervalRepath = 0.4f;
+    bool m_debugEnabled = true;
 
+private:
+    EnemyDetectionAggro* m_enemyDetectionAggro = nullptr;
+    NavigationState m_currentState = NavigationState::Idle;
+    Transform* m_currentTarget = nullptr;
+    float m_repathTimer = 0.0f;
 };
