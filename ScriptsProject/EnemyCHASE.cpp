16dc768936e192a9d144d8c7d681d55153cb3b3f
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-04-14 13:37:14 +0200

    updated with the new state machine script

---
+++ ScriptsProject/EnemyCHASE.cpp
@@ -0,0 +1,102 @@
+#include "pch.h"
+#include "EnemyCHASE.h"
+#include "EnemyController.h"
+
+static const ScriptFieldInfo CHASEFields[] =
+{
+	{ "Debug Enabled", ScriptFieldType::Bool, offsetof(EnemyCHASE, m_debugEnabled) }
+};
+
+IMPLEMENT_SCRIPT_FIELDS(EnemyCHASE, CHASEFields)
+
+
+EnemyCHASE::EnemyCHASE(GameObject* owner) : StateMachineScript(owner)
+{
+}
+
+void EnemyCHASE::OnStateEnter()
+{
+	Script* script = GameObjectAPI::getScript(getOwner(), "EnemyController");
+	m_enemyController = dynamic_cast<EnemyController*>(script);
+
+	Debug::log("EnemyCHASE Started");
+
+	if (!m_enemyController)
+	{
+		return;
+	}
+
+	m_enemyController->clearPath();
+	m_enemyController->resetRepathTimer();
+
+	if (m_debugEnabled)
+	{
+		Debug::log("[EnemyCHASE] ENTER");
+	}
+
+	m_enemyController->updateCurrentTarget();
+
+	if (m_enemyController->hasValidTarget())
+	{
+		m_enemyController->buildPathToTarget();
+	}
+}
+
+void EnemyCHASE::OnStateUpdate()
+{
+	if (!m_enemyController)
+	{
+		return;
+	}
+
+	AnimationComponent* animation = AnimationAPI::getAnimationComponent(getOwner());
+	if (!animation)
+	{
+		return;
+	}
+
+	m_enemyController->updateCurrentTarget();
+
+	if (!m_enemyController->hasValidTarget())
+	{
+		AnimationAPI::sendTrigger(animation, "Idle");
+		return;
+	}
+
+	if (m_enemyController->isTargetInCombatRange())
+	{
+		m_enemyController->faceCurrentTarget();
+		AnimationAPI::sendTrigger(animation, "Idle"); // need to trigger combat state
+		return;
+	}
+
+	m_enemyController->addToRepathTimer(Time::getDeltaTime());
+
+	if (m_enemyController->shouldRepath())
+	{
+		m_enemyController->buildPathToTarget();
+		m_enemyController->resetRepathTimer();
+	}
+
+	m_enemyController->followPath();
+}
+
+void EnemyCHASE::OnStateExit()
+{
+	if (!m_debugEnabled)
+	{
+		return;
+	}
+
+	Debug::log("[EnemyCHASE] EXIT");
+
+	if (!m_enemyController)
+	{
+		return;
+	}
+
+	m_enemyController->clearPath();
+	m_enemyController->resetRepathTimer();
+}
+
+IMPLEMENT_SCRIPT(EnemyCHASE)
---
+++ ScriptsProject/EnemyIDLE.cpp
@@ -0,0 +1,80 @@
+#include "pch.h"
+#include "EnemyIDLE.h"
+#include "EnemyController.h"
+
+static const ScriptFieldInfo IDLEFields[] =
+{
+	{ "Debug Enabled", ScriptFieldType::Bool, offsetof(EnemyIDLE, m_debugEnabled) }
+};
+
+IMPLEMENT_SCRIPT_FIELDS(EnemyIDLE, IDLEFields)
+
+
+EnemyIDLE::EnemyIDLE(GameObject* owner) : StateMachineScript(owner)
+{
+}
+
+void EnemyIDLE::OnStateEnter()
+{
+	Script* script = GameObjectAPI::getScript(getOwner(), "EnemyController");
+	m_enemyController = dynamic_cast<EnemyController*>(script);
+
+	if (!m_enemyController)
+	{
+		return;
+	}
+
+	m_enemyController->clearPath();
+	m_enemyController->resetRepathTimer();
+
+	if (m_debugEnabled)
+	{
+		Debug::log("[EnemyIDLE] ENTER");
+	}
+}
+
+void EnemyIDLE::OnStateUpdate()
+{
+	if (!m_enemyController)
+	{
+		return;
+	}
+
+	m_enemyController->updateCurrentTarget();
+
+	if (!m_enemyController->hasValidTarget())
+	{
+		return;
+	}
+
+	if (m_enemyController->isTargetInCombatRange())
+	{
+		m_enemyController->faceCurrentTarget();
+		return;
+	}
+
+	AnimationComponent* animation = AnimationAPI::getAnimationComponent(getOwner());
+	if (!animation)
+	{
+		return;
+	}
+
+	AnimationAPI::sendTrigger(animation, "chase");
+
+	if (m_debugEnabled)
+	{
+		Debug::log("[EnemyIDLE] Chase trigger sent");
+	}
+}
+
+void EnemyIDLE::OnStateExit()
+{
+	if (!m_debugEnabled)
+	{
+		return;
+	}
+
+	Debug::log("[EnemyIDLE] EXIT");
+}
+
+IMPLEMENT_SCRIPT(EnemyIDLE)
