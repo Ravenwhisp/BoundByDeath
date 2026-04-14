67ff07c21412a901ace465984e9c27e6c4266d93
Jordi Pi <pimoreno.j@outlook.es>
2026-04-14 13:48:44 +0200

    Player damageable includes proper animation calls

--- ScriptsProject/PlayerDamageable.h
+++ ScriptsProject/PlayerDamageable.h
@@ -2,6 +2,8 @@
 
 #include "Damageable.h"
 
+class PlayerAnimationController;
+
 class PlayerDamageable : public Damageable
 {
     DECLARE_SCRIPT(PlayerDamageable)
@@ -9,6 +11,14 @@ class PlayerDamageable : public Damageable
 public:
     explicit PlayerDamageable(GameObject* owner);
 
+    void Start() override;
+
 protected:
+    void onDamaged(float amount) override;
     void onHpDepleted() override;
+    void onDeath() override;
+    void onRevive() override;
+
+private:
+    PlayerAnimationController* m_playerAnimationController = nullptr;
 };
