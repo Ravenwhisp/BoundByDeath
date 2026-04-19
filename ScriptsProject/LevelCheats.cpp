7b2816310c5add8e6da02cabe4746e09e54110c2
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2026-04-19 20:26:57 +0200

    Traps & clean up part 2 & level cheats

--- ScriptsProject/LevelCheats.cpp
+++ ScriptsProject/LevelCheats.cpp
@@ -29,8 +29,8 @@ void LevelCheats::Update()
     if (KeyComboPressed(KeyCode::W)) AutoLose();
     if (KeyComboPressed(KeyCode::E)) Teleport();
     if (KeyComboPressed(KeyCode::R)) ActivateGodMode();
-    if (KeyComboPressed(KeyCode::T)) SpawnEnemies();
-    //if (KeyComboPressed(KeyCode::D)) restartLevel();
+    /*if (KeyComboPressed(KeyCode::T)) SpawnEnemies();*/
+    if (KeyComboPressed(KeyCode::D)) restartLevel();
     if (Input::isKeyDown(KeyCode::RightShift) && Input::isKeyDown(KeyCode::A))
     {
         if (KeyComboPressed(KeyCode::Num1)) 
@@ -144,11 +144,11 @@ void LevelCheats::DownState()
 {
     Debug::log("Down State activated! Player % i downed", m_playerIndex+1);
 	GameObject* player = SceneAPI::findAllGameObjectsByTag(Tag::PLAYER)[m_playerIndex];
-    Script* damageableScript = GameObjectAPI::getScript(player, "PlayerState");
-    PlayerState* playerState = dynamic_cast<PlayerState*>(damageableScript);
-    if (playerState)
+    Script* damageableScript = GameObjectAPI::getScript(player, "PlayerDamageable");
+    PlayerDamageable* damageable = dynamic_cast<PlayerDamageable*>(damageableScript);
+    if (damageable)
     {
-        playerState->setState(PlayerStateType::Downed);
+		damageable->takeDamage(damageable->getCurrentHp());
 	}
     
 }
@@ -156,7 +156,7 @@ void LevelCheats::DownState()
 void LevelCheats::restartLevel()
 {
     Debug::log("Restart Level activated!");
-    SceneAPI::requestSceneChange("LVL1Cheats");
+    SceneAPI::requestSceneChange("Level1");
 }
 
 IMPLEMENT_SCRIPT(LevelCheats)
