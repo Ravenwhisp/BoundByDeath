765dfd2ea60ccf327a7f5060935ff43762f46e2e
Jordi Pi <pimoreno.j@outlook.es>
2026-04-27 15:26:52 +0200

    powerup able to be collected by one specific character or both

--- ScriptsProject/PowerupCollectible.cpp
+++ ScriptsProject/PowerupCollectible.cpp
@@ -1,12 +1,19 @@
 #include "pch.h"
 #include "PowerupCollectible.h"
 
+static const char* powerupTargetNames[] =
+{
+    "Lyriel",
+    "Death",
+    "Both"
+};
+
+IMPLEMENT_SCRIPT_FIELDS(PowerupCollectible,
+    SERIALIZED_ENUM_INT(m_targetCharacter, "Target Character", powerupTargetNames, 3)
+)
+
 PowerupCollectible::PowerupCollectible(GameObject* owner)
     : Script(owner)
-{
-}
-
-void PowerupCollectible::Start()
 {
 }
 
@@ -22,11 +29,34 @@ void PowerupCollectible::OnTriggerEnter(GameObject* player)
         return;
     }
 
+    if (!canBeCollectedBy(player))
+    {
+        return;
+    }
+
     m_collected = true;
 
-    Debug::log("PowerUp collected");
-
     GameObjectAPI::removeGameObject(getOwner());
+}
+
+bool PowerupCollectible::canBeCollectedBy(GameObject* player) const
+{
+    const std::string& name = GameObjectAPI::getName(player);
+
+    switch (m_targetCharacter)
+    {
+    case LYRIEL:
+        return name == "Lyriel";
+
+    case DEATH:
+        return name == "Death";
+
+    case BOTH:
+        return name == "Lyriel" || name == "Death";
+
+    default:
+        return false;
+    }
 }
 
 IMPLEMENT_SCRIPT(PowerupCollectible)
--- ScriptsProject/PowerupCollectible.h
+++ ScriptsProject/PowerupCollectible.h
@@ -9,9 +9,21 @@ class PowerupCollectible : public Script
 public:
     explicit PowerupCollectible(GameObject* owner);
 
-    void Start() override;
     void OnTriggerEnter(GameObject* player) override;
 
+    ScriptFieldList getExposedFields() const override;
+
 private:
+    bool canBeCollectedBy(GameObject* other) const;
+
+private:
+    enum PowerUpTarget
+    {
+        LYRIEL = 0,
+        DEATH,
+        BOTH
+    };
+
+    int m_targetCharacter = BOTH;
     bool m_collected = false;
 };
