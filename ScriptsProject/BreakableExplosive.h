a05d26a012df430f7a762f14e23df7a117be47de
Javier Castaño Mansilla <javiercastanomansilla@gmail.com>
2026-05-12 17:36:51 +0200

    BreakableExplosive created

---
+++ ScriptsProject/BreakableExplosive.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ScriptAPI.h"
+
+#include "BreakableObject.h"
+
+class BreakableExplosive : public BreakableObject
+{
+    DECLARE_SCRIPT(BreakableExplosive)
+
+public:
+    explicit BreakableExplosive(GameObject* owner);
+
+    void Start() override;
+    void Update() override;
+
+    ScriptFieldList getExposedFields() const override;
+
+public:
+    float m_explosionRadius = 5.0f;
+    float m_explosionDamage = 30.0f;
+
+private:
+	void onBreak() override;
+
+};
