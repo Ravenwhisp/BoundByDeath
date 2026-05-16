ef3b31042a42952a7c649426909e521d20983eda
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-05-16 12:54:34 +0200

    Add boss target checks and pathfinding

--- ScriptsProject/ArthurBossController.cpp
+++ ScriptsProject/ArthurBossController.cpp
@@ -39,33 +39,122 @@ void ArthurBossController::Start()
 
 void ArthurBossController::drawGizmo()
 {
+	if (!m_debugEnabled)
+	{
+		return;
+	}
 
+	if (m_path.size() < 2)
+	{
+		return;
+	}
+
+	const Vector3 cyan = { 0.0f, 1.0f, 1.0f };
+	for (int i = 0; i < m_path.size() - 1; ++i)
+	{
+		DebugDrawAPI::drawLine(m_path[i], m_path[i + 1], cyan, 0, true);
+	}
 }
 
 bool ArthurBossController::hasValidTarget() const
 {
+	if (!m_arthurDetectionAggro)
+	{
+		return false;
+	}
+
+	Transform* targetTransform = m_arthurDetectionAggro->getCurrentTarget();
+	if (!targetTransform)
+	{
+		return false;
+	}
+
+	GameObject* targetObject = ComponentAPI::getOwner(targetTransform);
+	if (!targetObject)
+	{
+		return false;
+	}
+
+	Damageable* damageable = GameObjectAPI::findScript<Damageable>(targetObject);
+	if (damageable && damageable->isDead())
+	{
+		return false;
+	}
+
 	return true;
 }
 
 void ArthurBossController::updateCurrentTarget()
 {
+	if (!m_arthurDetectionAggro)
+	{
+		m_arthurDetectionAggro = GameObjectAPI::findScript<ArthurDetectionAggro>(getOwner());
+	}
 
+	if (!m_arthurDetectionAggro)
+	{
+		m_currentTarget = nullptr;
+		return;
+	}
+
+	m_currentTarget = m_arthurDetectionAggro->getCurrentTarget();
 }
 
 bool ArthurBossController::isTargetInCombatRange() const
 {
-	return true;
+	if (!m_currentTarget)
+	{
+		return false;
+	}
+
+	Vector3 ownerPosition = getOwner()->GetTransform()->getPosition();
+	Vector3 targetPosition = getOwner()->GetTransform()->getPosition();
+
+	Vector3 difference = ownerPosition - targetPosition;
+	difference.y = 0.0f;
+
+	return difference.Length() <= m_combatRange;
 }
 
 void ArthurBossController::clearPath()
 {
-	//todo
+	m_path.clear();
+	m_hasPath = false;
+	m_currentIndex = 0;
 }
 
 bool ArthurBossController::buildPathToTarget()
 {
-	//todo
-	return true;
+	if (!m_currentTarget)
+	{
+		return false;
+	}
+
+	Vector3 start = getOwner()->GetTransform()->getPosition();
+	Vector3 end = getChasePosition();
+
+	std::vector<Vector3> tempPath;
+	tempPath.resize(m_maxPathPoints);
+
+	int pointCount = NavigationAPI::findStraightPath(start, end, tempPath.data(), m_maxPathPoints, m_searchExtents, static_cast<NavAgentProfile>(m_enemyType));
+
+	if (pointCount > 0)
+	{
+		clearPath();
+
+		for (int i = 0; i < pointCount; ++i)
+		{
+			m_path.push_back(tempPath[i]);
+		}
+
+		m_currentIndex = 0;
+		m_hasPath = true;
+		return true;
+	}
+
+	clearPath();
+
+	return false;
 }
 
 void ArthurBossController::followPath()
