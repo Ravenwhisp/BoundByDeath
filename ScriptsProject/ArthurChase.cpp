b5e1d8b167aa6d7072d92e9cd17f46b99653b749
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2026-05-19 11:38:33 +0200

    Add ArthurChase state script -- unfinished

---
+++ ScriptsProject/ArthurChase.cpp
@@ -0,0 +1,85 @@
+#include "pch.h"
+#include "ArthurChase.h"
+
+#include "ArthurBossController.h"
+#include "ArthurAttackConfig.h"
+
+ArthurChase::ArthurChase(GameObject* owner)
+	: StateMachineScript(owner)
+{
+}
+
+void ArthurChase::OnStateEnter()
+{
+	m_arthurController = GameObjectAPI::findScript<ArthurBossController>(getOwner());
+	m_arthurAttackConfig = GameObjectAPI::findScript<ArthurAttackConfig>(getOwner());
+
+	if (!m_arthurController)
+	{
+		Debug::error("[ArthurChase] ArthurBossController not found.");
+		return;
+	}
+
+	if (!m_arthurAttackConfig)
+	{
+		Debug::error("[ArthurChase] ArthurAttackConfig not found.");
+		return;
+	}
+
+	m_arthurController->clearPath();
+	m_arthurController->resetRepathTimer();
+	m_arthurController->updateCurrentTarget();
+
+	if (m_arthurController->hasValidTarget())
+	{
+		m_arthurController->buildPathToTarget();
+	}
+
+	Debug::log("[ArthurChase] ENTER");
+}
+
+void ArthurChase::OnStateUpdate()
+{
+
+	if (!m_arthurController || !m_arthurAttackConfig)
+	{
+		return;
+	}
+
+	m_arthurController->updateCurrentTarget();
+
+	if (!m_arthurController->hasValidTarget())
+	{
+		m_arthurController->clearPath();
+		// go to idle state HERE
+		return;
+	}
+
+	m_arthurController->addToRepathTimer(Time::getDeltaTime());
+
+	if (m_arthurController->shouldRepath())
+	{
+		m_arthurController->buildPathToTarget();
+		m_arthurController->resetRepathTimer();
+	}
+
+	// attack checks + state transitions
+
+
+	m_arthurController->followPath();
+}
+
+void ArthurChase::OnStateExit()
+{
+	if (!m_arthurController)
+	{
+		return;
+	}
+
+	m_arthurController->clearPath();
+	m_arthurController->resetRepathTimer();
+
+	Debug::log("[ArthurChase] EXIT");
+}
+
+IMPLEMENT_SCRIPT(ArthurChase)
