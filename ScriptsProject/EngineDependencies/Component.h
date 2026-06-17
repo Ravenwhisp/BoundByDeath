3d569a17e13289804e1ca72e2521b059919c5a87
Jordi Pi <pimoreno.j@outlook.es>
2026-06-17 18:06:39 +0200

    ENGINE UPDATE 27

--- ScriptsProject/EngineDependencies/Component.h
+++ ScriptsProject/EngineDependencies/Component.h
@@ -7,7 +7,6 @@
 class Transform;
 class GameObject;
 class IArchive;
-
 class SceneReferenceResolver;
 
 class IDebugDrawable;
--- ScriptsProject/EngineDependencies/ScriptFieldHandler.h
+++ ScriptsProject/EngineDependencies/ScriptFieldHandler.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 class Script;
 class ScriptComponent;
 class SceneReferenceResolver;
