a890adba1d568529660a3bc5fba133b553cee802
Javier Castaño Mansilla <javiercastanomansilla@gmail.com>
2026-07-18 17:19:05 +0200

    CheckpointManager implemented (WIP)

---
+++ ScriptsProject/CheckpointManager.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "CheckpointManager.h"
+
+#include "CheckpointSetup.h"
+
+CheckpointManager::CheckpointManager(GameObject* owner)
+    : Script(owner)
+{
+}
+
+void CheckpointManager::Start()
+{
+}
+
+void CheckpointManager::Update()
+{
+}
+
+void CheckpointManager::ApplyCheckpoint()
+{
+	//cuando la escena se ha reseteado, y hay checkpoint guardado, recorremos los maps y aplicamos el estado guardado a cada objeto correspondiente
+
+	if(m_lastCheckpointId == CheckpointId::NONE)
+	{
+		return;
+	}
+
+	// 1. Apply hardcoded checkpoints
+	LoadCheckpointByID();
+
+	// 2. Apply saved states for dynamic objects
+}
+
+void CheckpointManager::LoadCheckpointByID()
+{
+	for (auto const& [id, setup] : m_checkpointSetupPool)
+	{
+		if(id > m_lastCheckpointId)
+		{
+			break;
+		}
+
+		if(setup != nullptr)
+		{
+			setup->ApplyCheckpointState();
+		}
+	}
+}
+
+IMPLEMENT_SCRIPT(CheckpointManager)
---
+++ ScriptsProject/CheckpointManager.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include "ScriptAPI.h"
+#include "PersistingPowerupState.h"
+#include <map>
+
+//////////////////////////////////////
+// Dynamic Checkpoint State Structures
+//////////////////////////////////////
+
+struct CharacterCheckpointState
+{
+	float m_lyrielHealth;
+    float m_deathHealth;
+    float m_reaperGaugeAmount;
+    bool m_unlockedPowerups[static_cast<int>(PowerupId::Count)];
+};
+
+struct EnemyCheckpointState
+{
+    //UID m_enemyId;
+    bool m_isDead;
+};
+
+struct BreakableCheckpointState
+{
+	//UID m_breakableId;
+    bool m_isBroken;
+};
+
+struct CollectibleCheckpointState
+{
+	//UID m_pickupId;
+    bool m_isCollected;
+};
+
+/////////////////////////////
+// Hardcoded Checkpoint State
+/////////////////////////////
+
+enum CheckpointId // Represents different checkpoints in the game, useful because they are partially harcoded
+{
+	NONE = 0,
+    CHECKPOINT_1_LEVEL_1= 100,
+    CHECKPOINT_2_LEVEL_1,
+    CHECKPOINT_3_LEVEL_1,
+
+	CHECKPOINT_1_LEVEL_2 = 200,
+
+	CHECKPOINT_1_LEVEL_3 = 300,
+	//... Add more checkpoints as needed
+};
+
+//struct CheckpointDef 
+//{
+//    Vector3 spawnPos;
+//
+//    std::vector<uint64_t> doorsToOpen;
+//    //puzzles superados, ver como guardar (id de puzle? y enviarselo al puzzle manager?)
+//};
+
+class CheckpointSetup;
+
+class CheckpointManager : public Script
+{
+    DECLARE_SCRIPT(CheckpointManager)
+
+public:
+    explicit CheckpointManager(GameObject* owner);
+
+    void Start() override;
+    void Update() override;
+
+    template <typename T>
+    void SaveState(UID id, const T& checkpointState)
+    {
+        if constexpr (std::is_same_v<T, CharacterCheckpointState>)
+        {
+            m_characterCheckpoints.insert_or_assign(id, checkpointState);
+        }
+        else if constexpr (std::is_same_v<T, EnemyCheckpointState>)
+        {
+            m_enemyCheckpoints.insert({ id, checkpointState });
+        }
+        else if constexpr (std::is_same_v<T, BreakableCheckpointState>)
+        {
+            m_breakableCheckpoints.insert({ id, checkpointState });
+        }
+        else if constexpr (std::is_same_v<T, CollectibleCheckpointState>)
+        {
+            m_collectibleCheckpoints.insert({ id, checkpointState });
+        }
+        else
+        {
+			//assert(false && "Unsupported checkpoint state type");
+        }
+    }
+
+	void SetCheckpointId(CheckpointId checkpointId) { m_lastCheckpointId = checkpointId; }
+
+    void RegisterCheckpoint(const CheckpointId& checkpointId, CheckpointSetup* setup)
+    {
+        m_checkpointSetupPool[checkpointId] = setup;
+    }
+
+    void LoadCheckpointByID();
+
+    void ApplyCheckpoint();
+
+private:
+	CheckpointId m_lastCheckpointId = CheckpointId::NONE;
+
+	std::map<CheckpointId, CheckpointSetup*> m_checkpointSetupPool;
+
+	std::map<UID, CharacterCheckpointState> m_characterCheckpoints;
+    std::map<UID, EnemyCheckpointState> m_enemyCheckpoints;
+    std::map<UID, BreakableCheckpointState> m_breakableCheckpoints;
+    std::map<UID, CollectibleCheckpointState> m_collectibleCheckpoints;
+};
