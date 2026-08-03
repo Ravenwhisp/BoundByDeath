704c88f9567329cd44c404b0e426ead34221230e
Raul <raul.rodriguez.trotonda@gmail.com>
2026-08-03 13:44:39 +0200

    Dissolve applied

--- ScriptsProject/EnemyDamageable.cpp
+++ ScriptsProject/EnemyDamageable.cpp
@@ -21,7 +21,10 @@ IMPLEMENT_SCRIPT_FIELDS_INHERITED(EnemyDamageable, Damageable,
 	SERIALIZED_FLOAT(m_shadowExecutionPreviewFadeTime, "Shadow Preview Fade Time", 0.0f, 2.0f, 0.05f),
 	SERIALIZED_FLOAT(m_shadowExecutionPreviewHitTime, "Shadow Preview Hit Time", 0.05f, 1.0f, 0.05f),
 	SERIALIZED_FLOAT(m_shadowExecutionPreviewNonLethalAlpha, "Shadow Preview Non-Lethal Alpha", 0.0f, 1.0f, 0.05f),
-	SERIALIZED_FLOAT(m_shadowExecutionPreviewLethalAlpha, "Shadow Preview Lethal Alpha", 0.0f, 1.0f, 0.05f)
+	SERIALIZED_FLOAT(m_shadowExecutionPreviewLethalAlpha, "Shadow Preview Lethal Alpha", 0.0f, 1.0f, 0.05f),
+	FIELD_GROUP_LABEL("DissolveEffect"),
+	SERIALIZED_COMPONENT_REF(m_renderer, "Mesh Renderer", ComponentType::TRANSFORM),
+	SERIALIZED_FLOAT(m_dissolveDuration, "Dissolve Duration", 0.1f, 5.0f, 0.0f)
 )
 
 EnemyDamageable::EnemyDamageable(GameObject* owner)
@@ -85,6 +88,8 @@ void EnemyDamageable::Start()
 	}
 
 	setHealthBarAlpha(0.0f);
+
+	loadDissolveComponent();
 }
 
 void EnemyDamageable::Update()
@@ -93,6 +98,11 @@ void EnemyDamageable::Update()
 	updateHealthBarFade();
 	updateShadowExecutionPreviewAvailability();
 	updateShadowExecutionPreviewAnimation(Time::getDeltaTime());
+
+	if (m_dissolve != nullptr && m_dissolveActive)
+	{
+		updateDissolveEffect();
+	}
 }
 
 void EnemyDamageable::takeDamage(const HitContext& ctx)
@@ -203,6 +213,8 @@ void EnemyDamageable::onDeath()
 	Damageable::onDeath();
 
 	setShadowExecutionThresholdMarkerVisible(false);
+
+	m_dissolveActive = true;
 
 	if (m_shadowMark)
 	{
@@ -357,6 +369,20 @@ void EnemyDamageable::updateHealthBarFade()
 		setHealthBarAlpha(1.0f);
 		m_healthBarFadeActive = false;
 	}
+}
+
+void EnemyDamageable::updateDissolveEffect()
+{
+	m_dissolveTimer += (Time::getDeltaTime());
+	if (m_dissolveTimer >= m_dissolveDuration)
+	{
+		m_dissolveTimer = m_dissolveDuration;
+		m_dissolveActive = false;
+	}
+
+	float amount = m_dissolveTimer / m_dissolveDuration;
+
+	ShadersAPI::setDissolveAmount(m_dissolve, amount);
 }
 
 void EnemyDamageable::setHealthBarAlpha(float alpha)
@@ -578,6 +604,25 @@ void EnemyDamageable::updateShadowExecutionThresholdMarker()
 	const float markerX = leftEdge + containerSize.x * thresholdPercent;
 
 	Transform2DAPI::setPosition(m_shadowExecutionThresholdMarkerTransform, Vector2(markerX, markerPosition.y));
+}
+
+void EnemyDamageable::loadDissolveComponent()
+{
+	Transform* rendererTransform = m_renderer.getReferencedComponent();
+
+	if (rendererTransform == nullptr)
+	{
+		Debug::warn("EnemyDamageable on '%s' has a missing renderer reference.", GameObjectAPI::getName(getOwner()));
+	}
+	else
+	{
+		m_dissolve = ShadersAPI::getDissolveComponent(ComponentAPI::getOwner(rendererTransform));
+
+		if (m_dissolve == nullptr)
+		{
+			Debug::warn("Renderer referenced in EnemyDamageable on '%s' does not have a Dissolve component.", GameObjectAPI::getName(getOwner()));
+		}
+	}
 }
 
 IMPLEMENT_SCRIPT(EnemyDamageable)
